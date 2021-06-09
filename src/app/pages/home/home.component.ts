74e216b9678c181bc35f5cf60514b97405a5f54c
Emerson <emerson_sardinha@hotmail.com>
2021-06-08 22:00:21 -0300

    Corrigindo links ancora da tela Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,4 +12,29 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //metodo mostra as informações na tela Home de acordo com item clicado
+  //criado para corrigir bug de link ancora que não estava funcionando inicialmente
+  //com esta implementação, o evento perdeu propriedades como o FADE
+  showInfo(id: string, event: Event) {
+
+    //LIMPA TODAS AS SELEÇÕES
+    document.querySelectorAll("div[role='tabpanel']").forEach(el => {
+      el.setAttribute("class", "tab-pane fade")
+    })
+
+    //HABILITA ELEMENTO
+    let el = document.getElementById(id)!
+    el.setAttribute("class", "tab-pane fade show active")
+
+
+    //LIMPA TODAS AS SELEÇÕES
+    document.querySelectorAll("a[class*='list-group-item-action']").forEach(el => {
+      el.setAttribute("class", "list-group-item list-group-item-action")
+    })
+
+    //AJUSTANDO MENU
+    let item = event.target as Element;
+    item.setAttribute("class", "list-group-item list-group-item-action active")
+
+  }
 }
