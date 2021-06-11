61b9ec2068c7a72cec714369e6cc77c9e9020409
Emerson Melo <emerson_sardinha@hotmail.com>
2021-06-11 10:53:41 -0300

    Criando HTML departamentos/cadastrar com rota e ajustes diversos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartamentoCadastroComponent } from './pages/departamento/departamento-cadastro/departamento-cadastro.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'departamentos/cadastrar', component: DepartamentoCadastroComponent}
 ];
 
 @NgModule({
