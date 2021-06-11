4dd919c6ea44d6b42b12d1941960553fdcaffc76
Emerson Melo <emerson_sardinha@hotmail.com>
2021-06-11 11:33:27 -0300

    Criando HTMLs e rotas para cargos e funcionarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CargoCadastroComponent } from './pages/cargo/cargo-cadastro/cargo-cadastro.component';
+import { CargoListaComponent } from './pages/cargo/cargo-lista/cargo-lista.component';
 import { DepartamentoCadastroComponent } from './pages/departamento/departamento-cadastro/departamento-cadastro.component';
 import { DepartamentoListaComponent } from './pages/departamento/departamento-lista/departamento-lista.component';
+import { FuncionarioCadastroComponent } from './pages/funcionario/funcionario-cadastro/funcionario-cadastro.component';
+import { FuncionarioListaComponent } from './pages/funcionario/funcionario-lista/funcionario-lista.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'departamentos/cadastrar', component: DepartamentoCadastroComponent},
-  {path: 'departamentos/listar', component: DepartamentoListaComponent}
+  {path: 'departamentos/listar', component: DepartamentoListaComponent},
+  {path: 'cargos/cadastrar', component: CargoCadastroComponent},
+  {path: 'cargos/listar', component: CargoListaComponent},
+  {path: 'funcionarios/cadastrar', component: FuncionarioCadastroComponent},
+  {path: 'funcionarios/listar', component: FuncionarioListaComponent},
 ];
 
 @NgModule({
