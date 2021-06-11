49d8ea834c6ccf73cee357af05274b66f8de4817
Emerson Melo <emerson_sardinha@hotmail.com>
2021-06-11 11:09:24 -0300

    Criacao do HTML e rota para departamentos/listar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartamentoCadastroComponent } from './pages/departamento/departamento-cadastro/departamento-cadastro.component';
+import { DepartamentoListaComponent } from './pages/departamento/departamento-lista/departamento-lista.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'departamentos/cadastrar', component: DepartamentoCadastroComponent}
+  {path: 'departamentos/cadastrar', component: DepartamentoCadastroComponent},
+  {path: 'departamentos/listar', component: DepartamentoListaComponent}
 ];
 
 @NgModule({
