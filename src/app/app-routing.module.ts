4a1df073c60cb23f3afa594ab6d2ed6ae51b5306
Emerson <emerson_sardinha@hotmail.com>
2021-07-21 20:14:36 -0300

    Ajustes finais - Checando tipo de dispositivo com ngx-device-detector e descrevendo tecnologias em Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,30 +9,6 @@ const routes: Routes = [
   { path: 'departamentos', loadChildren: () => import('./pages/departamento/departamentos.module').then(m => m.DepartamentosModule) },
   { path: 'cargos', loadChildren: () => import('./pages/cargo/cargo.module').then(m => m.CargosModule) },
   { path: 'funcionarios', loadChildren: () => import('./pages/funcionario/funcionario.module').then(m => m.FuncionarioModule) },
-  // {
-  //   path: 'departamentos/editar/:id', component: DepartamentoCadastroComponent,
-  //   resolve: {
-  //     departamentoDTO: DepartamentoResolver
-  //   }
-  // },
-  // { path: 'cargos/cadastrar', component: CargoCadastroComponent },
-  // { path: 'cargos/listar', component: CargoListaComponent },
-  // {
-  //   path: 'cargos/editar/:id', component: CargoCadastroComponent,
-  //   resolve: {
-  //     // o atributo "cargoResolver" esta ligado diretamente
-  //     // com o atributo setado no metodo "preEdit" do cargo-cadastro.component
-  //     cargoResolver: CargoResolverGuard
-  //   }
-  // },
-  // { path: 'funcionarios/cadastrar', component: FuncionarioCadastroComponent },
-  // { path: 'funcionarios/listar', component: FuncionarioListaComponent },
-  // {
-  //   path: 'funcionarios/editar/:id', component: FuncionarioCadastroComponent,
-  //   resolve: {
-  //     funcionarioResolver: FuncionarioResolverGuard
-  //   }
-  // },
   { path: 'error', component: ErrorComponent }
 ];
 
--- src/app/fragments/sidebar/sidebar.component.ts
+++ src/app/fragments/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@
 import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
+import { DeviceDetectorService } from 'ngx-device-detector';
 import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
@@ -11,13 +12,30 @@ import { SidebarService } from 'src/app/services/sidebar.service';
 })
 export class SidebarComponent implements OnInit {
 
+  public isDesktop: boolean = true;
 
-  @ViewChild('drawer', {static: true}) public drawer!: MatDrawer;
+  @ViewChild('drawer', {static: true})
+  public drawer!: MatDrawer;
 
-  constructor(private sidebarService: SidebarService) { }
+  constructor(private sidebarService: SidebarService,
+    private deviceService: DeviceDetectorService) { }
 
   ngOnInit(): void {
     this.sidebarService.setDrawer(this.drawer);
+    this.checkTypeDevice();
+  }
+
+  // caso seja mobile, inicia com sidebar recuado
+  checkTypeDevice(): boolean {
+    const isMobile = this.deviceService.isMobile();
+
+    if(isMobile){
+      this.isDesktop = false;
+      return this.isDesktop;
+    } else {
+      this.isDesktop = true;
+      return this.isDesktop;
+    }
   }
 
 }
